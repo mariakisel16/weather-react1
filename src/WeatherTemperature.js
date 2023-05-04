425031299f24271e4eb6323ba235d65cf730a4ce
mariakisel16 <maria.kisel16@yahoo.com>
2023-05-04 11:20:57 -0400

    made changes
    added WeatherTemperature, created C and Fahrenheit to convert

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  function converToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function convertToCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="weatherTemeperature">
+        <span className="weatherTemperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={converToFahrenheit}>
+            {" "}
+            °F{" "}
+          </a>{" "}
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="weatherTemeperature">
+        <span className="weatherTemperature">{Math.round(fahrenheit)}</span>
+        <span className="unit">
+          {" "}
+          <a href="/" onClick={converToCelsius}>
+            °C{" "}
+          </a>
+          | °F{" "}
+        </span>
+      </div>
+    );
+  }
+}
