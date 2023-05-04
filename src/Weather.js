3628bc23d1a2d585ea746af9174f0444f919de7d
mariakisel16 <maria.kisel16@yahoo.com>
2023-05-04 11:49:33 -0400

    i

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherIcon from "./WeatherIcon";
-import WeatherTemperature from ".WeatherTemperature";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function Weather() {
   let [city, setCity] = useState(" ");
